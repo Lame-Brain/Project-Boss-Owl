5eb35c9c34901d490804f78a08708772ce4e3ffc
brett.roberts <brett.roberts48@gmail.com>
2019-08-07 21:02:03 -0700

    Basic map memory alloc and Game Manager added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿/**********************************************************************************************************************************************************************************************************************
+* Author:		Brett Roberts
+* Filename:		GameManager.cs
+* Date Created: 8/7/2019
+*
+* Class: GameManager
+*
+* Purpose: Stores global properties and instances for the game such as the Map and Player 
+*
+* Methods:
+*       
+***********************************************************************************************************************************************************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager m_instance;
+
+    //Map Globals
+    public int SIZE_OVERWORLD_X = 5;
+    public int SIZE_OVERWORLD_Y = 5;
+
+    private void Awake()
+    {
+        m_instance = this;
+    }
+
+    public OverworldMap m_overworldMap;
+}
---
+++ Assets/Scripts/MapStuff/cOverworldMap/OverworldMap.cs
@@ -0,0 +1,36 @@
+﻿/**********************************************************************************************************************************************************************************************************************
+* Author:		Brett Roberts
+* Filename:		OverworldMap.cs
+* Date Created: 8/7/2019
+*
+* Class: OverworldMap
+*
+* Purpose: Main map that the player will traverse as they move between locations (dungeons, caves, etc...)
+*
+* Manager functions:
+*	OverworldMap()
+*		Initializes the 2D list
+* Methods:
+*       
+***********************************************************************************************************************************************************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldMap : MonoBehaviour
+{
+    private List<List<Location>> m_list;
+
+    //Default ctor to allocate memory for m_list
+    public OverworldMap()
+    {
+        //Create a list of lists (2D list "array") that will be as large as the X-axis
+        m_list = new List<List<Location>>(GameManager.m_instance.SIZE_OVERWORLD_X);
+
+        //For each list within m_list create a list that will be as large as the Y-axis
+        for (int i = 0; i < GameManager.m_instance.SIZE_OVERWORLD_X; ++i)
+            m_list[i] = new List<Location>(GameManager.m_instance.SIZE_OVERWORLD_Y);
+    }
+
+
+}
