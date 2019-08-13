00479b8a2a245499f12358a0705cb4e90c2567a2
Ethan Roberts <theLameBrain@gmail.com>
2019-08-13 13:19:30 -0700

    Hooked State Machines up to existing Chracters

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -16,17 +16,18 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public int m_xCoord, m_yCoord;
-    public bool m_isMoving;
+    private bool m_isMoving;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _animator.SetBool("isMoving", m_isMoving);
     }
 }
