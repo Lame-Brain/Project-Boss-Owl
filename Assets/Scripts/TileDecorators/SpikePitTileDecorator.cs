10e5073bacbc8a0862e79d96f1ff579d0c3fb5fb
brett.roberts <brett.roberts48@gmail.com>
2020-06-19 03:28:34 -0700

    Added extra comments to decorator classes

--- Assets/Scripts/TileDecorators/SpikePitTileDecorator.cs
+++ Assets/Scripts/TileDecorators/SpikePitTileDecorator.cs
@@ -1,9 +1,9 @@
 ﻿/**********************************************************************************************************************************************************************************************************************
 * Author:		Brett Roberts
-* Filename:		DoorTileDecorator.cs
-* Date Created: 6/18/2020
+* Filename:		SpikePitDecorator.cs
+* Date Created: 6/19/2020
 *
-* Class: DoorTileDecorator
+* Class: SpikePitDecorator
 *
 * Purpose: Defines a door decorator to place on tiles
 *   (Concrete Decorator for Tile as part of Decorator Pattern)
@@ -11,8 +11,6 @@
 * Properties:
 *   public override bool IsBlockedGround
 *       Returns whether the door is blocking (closed is blocking, open is not)
-*   public DecoratorSpriteId
-*       Returns the spriteId for a closed door if the door is closed, or an open one if the door is opened
 * Methods:
 *  public void Open()
 *   Sets the door to not be closed and changes sprite to an open door
@@ -24,45 +22,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorTileDecorator : TileDecorator
+public class SpikePitDecorator : TileDecorator
 {
-    protected bool _isClosed;
-
     private void Awake()
     {
-        _isClosed = false;
+            
     }
 
-    public override bool IsBlockedGround
+    public override int Damage
     {
-        get { return _isClosed; }
+        get { return base.Damage + CalcDamage(); }
     }
 
-    //******To be fully implemented
-    public override int DecoratorSpriteId
+    //Calculate the spike pit will deal
+    protected int CalcDamage()
     {
-        get
-        {
-            //Return the closed door sprite id
-            if (_isClosed)
-            {
-                return -1;
-            }
-            //Return the open door sprite id
-            else
-            {
-                return -1;
-            }
-        }
-    }
-
-    public void Open()
-    {
-        _isClosed = false;
-    }
-
-    public void Close()
-    {
-        _isClosed = true;
+        return 5;
     }
 }
